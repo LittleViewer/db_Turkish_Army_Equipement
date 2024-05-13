4de6a6e998b51a0f995855b8fac3a90671c769cf
LittleViewer <planete.adoecrivain@gmail.com>
2024-05-13 22:20:15 +0200

    Update V.1_Forcee_armee_turque_database.sql

--- V.1_Forcee_armee_turque_database.sql
+++ V.1_Forcee_armee_turque_database.sql
@@ -1,25 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:3306
--- Généré le : lun. 13 mai 2024 à 20:18
--- Version du serveur : 8.0.36-0ubuntu0.22.04.1
--- Version de PHP : 8.1.2-1ubuntu2.17
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `Forcee_armee_turque_database`
---
 
 -- --------------------------------------------------------
 
